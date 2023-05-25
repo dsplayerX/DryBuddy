7b71ae1ae113fa937dd3c962d08585dc868e3689
Dumindu <100945633+dsplayerX@users.noreply.github.com>
2023-05-25 13:05:07 +0530

    created a simple weather app
    it displays weather information of the current geolocation. loads api key from secrets.yaml

--- flutterapp/drybuddy/lib/main.dart
+++ flutterapp/drybuddy/lib/main.dart
@@ -1,125 +1,226 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:yaml/yaml.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(DryBuddyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<String> loadApiKey() async {
+  final secretFile = await rootBundle.loadString('assets/secrets.yaml');
+  final secrets = loadYaml(secretFile);
+  return secrets['OPENWEATHER_API_KEY'] as String;
+}
 
-  // This widget is the root of your application.
+class DryBuddyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'DryBuddy Demo Home Page'),
+      title: 'DryBuddy App',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: WeatherHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class WeatherHomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _WeatherHomePageState createState() => _WeatherHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _WeatherHomePageState extends State<WeatherHomePage> {
+  String _cityName = '';
+  String _temperature = '';
+  String _weatherDescription = '';
+  String _latitude = '';
+  String _longitude = '';
+  String _feelsLike = '';
+  bool _isLoading = false;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeatherData();
+  }
+
+  void _fetchWeatherData() async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isLoading = true;
     });
+
+    // Check if location permission is granted
+    final locationPermissionStatus = await Permission.locationWhenInUse.status;
+    if (locationPermissionStatus.isGranted) {
+      try {
+        Position position = await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high);
+        // Fetch weather data using the obtained latitude and longitude
+        print('Latitude: ${position.latitude}');
+        print('Longitude: ${position.longitude}');
+
+        _fetchWeatherDataByLocation(position.latitude, position.longitude);
+      } catch (error) {
+        setState(() {
+          _isLoading = false;
+          _weatherDescription =
+              'Error fetching data. Please enable location access or try again later.';
+        });
+        // Show a dialog or snackbar to inform the user and provide an alternative method to fetch weather data
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Location Access Denied'),
+              content:
+                  Text('Please enable location access or try again later.'),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+        return;
+      }
+    } else {
+      // Location permission denied, request permission
+      final permissionStatus = await Permission.locationWhenInUse.request();
+      if (permissionStatus.isGranted) {
+        try {
+          Position position = await Geolocator.getCurrentPosition(
+              desiredAccuracy: LocationAccuracy.high);
+          // Fetch weather data using the obtained latitude and longitude
+          _fetchWeatherDataByLocation(position.latitude, position.longitude);
+        } catch (error) {
+          setState(() {
+            _isLoading = false;
+            _weatherDescription =
+                'Error fetching data. Please enable location access or try again later.';
+          });
+          // Show a dialog or snackbar to inform the user and provide an alternative method to fetch weather data
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Location Access Denied'),
+                content:
+                    Text('Please enable location access or try again later.'),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text('OK'),
+                  ),
+                ],
+              );
+            },
+          );
+          return;
+        }
+      } else {
+        setState(() {
+          _isLoading = false;
+          _weatherDescription =
+              'Error fetching data. Please enable location access or try again later.';
+        });
+        return;
+      }
+    }
+  }
+
+  void _fetchWeatherDataByLocation(double latitude, double longitude) async {
+    final apiKey = await loadApiKey();
+    final apiUrl =
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey';
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+
+        final mainData = data['main'];
+        final weatherData = data['weather'][0];
+
+        setState(() {
+          _cityName = data['name'];
+          _temperature = (mainData['temp'] - 273.15).toStringAsFixed(2);
+          _weatherDescription = weatherData['description'];
+          _latitude = latitude.toStringAsFixed(2);
+          _longitude = longitude.toStringAsFixed(2);
+          _feelsLike = (mainData['feels_like'] - 273.15).toStringAsFixed(2);
+        });
+      } else {
+        setState(() {
+          _cityName = '';
+          _temperature = '';
+          _weatherDescription = 'Error fetching data';
+          _latitude = '';
+          _longitude = '';
+          _feelsLike = '';
+        });
+      }
+    } catch (error) {
+      setState(() {
+        _cityName = '';
+        _temperature = '';
+        _weatherDescription = 'Error fetching data';
+        _latitude = '';
+        _longitude = '';
+        _feelsLike = '';
+      });
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _refreshWeatherData() {
+    setState(() {
+      _cityName = '';
+      _temperature = '';
+      _weatherDescription = '';
+      _isLoading = true;
+    });
+
+    _fetchWeatherData();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('DryBuddy App'),
+        actions: [
+          ElevatedButton(
+            onPressed: _refreshWeatherData,
+            child: Icon(Icons.refresh),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          _isLoading
+              ? CircularProgressIndicator()
+              : Column(
+                  children: [
+                    Text('City: $_cityName'),
+                    Text('Temperature: $_temperature °C'),
+                    Text('Description: $_weatherDescription'),
+                    Text('Latitude: $_latitude'),
+                    Text('Longitude: $_longitude'),
+                    Text('Feels Like: $_feelsLike °C'),
+                  ],
+                ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
